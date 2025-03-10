7e3edcb85191690b12b6fbae4958f6649b18bad2
Jaydip Patel <jaypat7828@gmail.com>
2025-03-10 15:19:21 +0000

    Update the app - improvement.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ---
-title: "TextSummarization"
+title: "Text Summarization"
 emoji: 😊
-colorFrom: "orange"
+colorFrom: "pink"
 colorTo: "blue"
 sdk: "gradio"
 sdk_version: "5.20.1"
@@ -9,6 +9,40 @@ app_file: app.py
 pinned: false
 ---
 
-# summarization
+[![Sync to Hugging Face hub](https://github.com/jaypat7828/summarization/actions/workflows/main.yml/badge.svg?branch=main)](https://github.com/jaypat7828/summarization/actions/workflows/main.yml)
 
-This is an App to create a codespace using hugging face free llm models.
+# Text Summarization App
+
+Welcome to the Text Summarization App! This application leverages the power of Hugging Face's BART model to provide high-quality text summarization. The app is built using Gradio, a user-friendly interface for machine learning models.
+
+## Features
+
+- **Input Text**: Enter the text you want to summarize.
+- **Summarize**: Click the "🔍 Summarize" button to generate a summary of the input text.
+- **Reset**: Clear the input and output fields with the "🔄 Reset" button.
+- **Provide Feedback**: Provide feedback on the generated summary with the "👍 Provide Feedback" button.
+- **Load Example**: Load a sample text to see how the summarization works.
+
+## How to Use
+
+1. **Enter Text**: Type or paste the text you want to summarize into the "Input Text" box.
+2. **Summarize**: Click the "🔍 Summarize" button to generate a summary.
+4. **Reset**: Click the "🔄 Reset" button to clear the input and output fields.
+5. **Feedback**: Provide feedback on the summary using the "👍 Provide Feedback" button.
+6. **Example**: Click the "📄 Load Example" button to load a sample text.
+
+## Model
+
+This app uses the [BART model](https://huggingface.co/facebook/bart-large-cnn) from Hugging Face, which is a powerful transformer model for text summarization.
+
+## Link to Space
+Visit the Hugging Facen [App](https://huggingface.co/spaces/JayPat78322/TextSummarization).
+
+## Acknowledgements
+
+- **Hugging Face**: For providing the BART model and the transformers library.
+- **Gradio**: For the easy-to-use interface for machine learning models.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
--- app.py
+++ app.py
@@ -2,21 +2,76 @@ import gradio as gr
 from transformers import pipeline
 
 # Load the model
-model = pipeline("summarization")
+model = pipeline("summarization", model="facebook/bart-large-cnn")
+
+# Load example text
+with open("example.txt", "r", encoding="utf-8") as file:
+    example_text = file.read()
 
 
-def predict(text):
-    return model(text)[0]["summary_text"]
+def predict(text, max_length=200):
+    summary = model(text, max_length=max_length)[0]["summary_text"]
+    return summary
+
+
+def reset():
+    return "", "", "", gr.update(interactive=True)
+
+
+def get_token_length(text):
+    token_length = len(text.split())
+    color = "red" if token_length > 750 else "green"
+    button_state = gr.update(interactive=token_length <= 750)
+    return (
+        f'<span style="color:{color}">Input Token Length: {token_length}</span>',
+        button_state,
+    )
+
+
+def load_example():
+    return example_text
 
 
 # Create a Gradio interface to summarize text with a text box input
 with gr.Blocks() as demo:
+    gr.Markdown("# 📝 Text Summarization App")
+    gr.Markdown("### ✨ Enter text to get a summary")
+
+    load_example_button = gr.Button("📄 Load Example", size="sm")
+    token_length_label = gr.HTML(value="Input Token Length: 0")
     input_text = gr.Textbox(label="Input Text")
+    with gr.Row():
+        summarize_button = gr.Button("🔍 Summarize", interactive=True, size="medium")
+        reset_button = gr.Button("🔄 Reset", size="medium")
     output_text = gr.Textbox(label="Summary")
-    summarize_button = gr.Button("Summarize")
+    with gr.Row():
+        feedback_button = gr.Button("👍 Provide Feedback", size="medium")
 
-    summarize_button.click(
-        fn=predict, inputs=input_text, outputs=output_text
-    )  # pylint: disable=no-member
+    input_text.change(  # pylint: disable=E1101
+        fn=get_token_length,
+        inputs=input_text,
+        outputs=[token_length_label, summarize_button],
+    )
 
-demo.launch()
+    summarize_button.click(  # pylint: disable=E1101
+        fn=predict,
+        inputs=input_text,
+        outputs=output_text,
+    )
+    reset_button.click(  # pylint: disable=E1101
+        fn=reset,
+        inputs=None,
+        outputs=[input_text, output_text, token_length_label, summarize_button],
+    )
+
+    load_example_button.click(  # pylint: disable=E1101
+        fn=load_example,
+        inputs=None,
+        outputs=input_text,
+    )
+
+    gr.Markdown(
+        "[Visit Bart Model](https://huggingface.co/facebook/bart-large-cnn) | [Visit Project](https://github.com/jaypat7828/summarization)"
+    )
+
+demo.launch(share=True)
